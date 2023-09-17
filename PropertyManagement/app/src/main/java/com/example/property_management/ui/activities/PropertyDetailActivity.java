99f63923d6bbfe002d296c5f4b24059eb30586f6
Lan Lin <497152365@qq.com>
2023-09-17 23:24:58 +1000

    add info to property details

--- PropertyManagement/app/src/main/java/com/example/property_management/ui/activities/PropertyDetailActivity.java
+++ PropertyManagement/app/src/main/java/com/example/property_management/ui/activities/PropertyDetailActivity.java
@@ -2,8 +2,10 @@ package com.example.property_management.ui.activities;
 
 import android.app.ActivityOptions;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -40,6 +42,7 @@ public class PropertyDetailActivity extends AppCompatActivity {
         // Create an ArrayList of image URLs (you can replace these with your actual URLs)
         ArrayList<String> imageUrls = new ArrayList<>();
 
+        // TODO fetch imageUrls from firebase
         imageUrls.add("https://images.unsplash.com/photo-1668889716746-fd2ca90373f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=900&q=60");
         imageUrls.add("https://images.unsplash.com/photo-1614174124242-4b3656523295?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=900&q=60");
         imageUrls.add("https://images.unsplash.com/photo-1694449263303-a90c4ce18112?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=900&q=60");
@@ -55,6 +58,23 @@ public class PropertyDetailActivity extends AppCompatActivity {
             }
         });
         recyclerView.setAdapter(adapter);
+
+        // linkButton
+        // Find the TextView by its ID
+        Button linkButton = findViewById(R.id.linkButton);
+        linkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Define the URL you want to open
+                String url = "https://www.domain.com.au/410-673-latrobe-street-docklands-vic-3008-16651885"; // Replace with your desired URL
+
+                // Create an Intent to open a web browser with the specified URL
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+
+                // Start the web browser activity
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
