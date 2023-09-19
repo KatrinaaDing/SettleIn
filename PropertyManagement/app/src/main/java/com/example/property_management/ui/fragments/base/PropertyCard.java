c85ee88e7dfb65ca0ffa52dc3a3a30832d8b4a09
KatrinaaDing <xiaoding1998@hotmail.com>
2023-09-19 22:11:08 +1000

    create a property card and amenities Group

---
+++ PropertyManagement/app/src/main/java/com/example/property_management/ui/fragments/base/PropertyCard.java
@@ -0,0 +1,55 @@
+package com.example.property_management.ui.fragments.base;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
+import com.example.property_management.R;
+import com.example.property_management.ui.fragments.property.AmenitiesGroup;
+import com.google.android.material.card.MaterialCardView;
+
+public class PropertyCard extends MaterialCardView {
+    private String address = "No Address Available";
+    private int price;
+    private String thumbnail = "https://th.bing.com/th/id/OIP.ou4JatDxkOZ1P9sZKyjxjAHaFj?pid=ImgDet&rs=1";
+    private boolean isInspected;
+
+    public PropertyCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.custom_property_card, this, true);
+
+        // get amentities group
+        AmenitiesGroup amenitiesGroup = findViewById(R.id.amenitiesGroup);
+
+        // set attributes
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.PropertyCard);
+        try {
+            this.price = a.getInteger(R.styleable.PropertyCard_price, 203);
+            int bedrooms = a.getInteger(R.styleable.PropertyCard_bedrooms, 0);
+            int bathrooms = a.getInteger(R.styleable.PropertyCard_bathrooms, 0);
+            int parkings = a.getInteger(R.styleable.PropertyCard_parkings, 0);
+            amenitiesGroup.setValues(bedrooms, bathrooms, parkings);
+            this.isInspected = a.getBoolean(R.styleable.PropertyCard_isInspected, false);
+            this.thumbnail = a.getString(R.styleable.PropertyCard_thumbnail);
+            if (a.hasValue(R.styleable.PropertyCard_address)){
+                this.address = a.getString(R.styleable.PropertyCard_address);
+            }
+            setValue();
+        } finally {
+            a.recycle();
+        }
+    }
+
+    private void setValue() {
+        TextView addressView = findViewById(R.id.addressTextView);
+        TextView priceView = findViewById(R.id.priceTextView);
+        addressView.setText(this.address);
+        if (this.price == -1)
+            priceView.setText("Price not available");
+        else
+            priceView.setText("$" + String.valueOf(this.price));
+
+    }
+}
