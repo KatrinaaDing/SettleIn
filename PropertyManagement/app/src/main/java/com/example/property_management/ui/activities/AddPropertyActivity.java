220b6361eedcda4437e375b5755972e161be8d77
KatrinaaDing <xiaoding1998@hotmail.com>
2023-11-04 23:21:14 +1100

    clean libraries

--- PropertyManagement/app/src/main/java/com/example/property_management/ui/activities/AddPropertyActivity.java
+++ PropertyManagement/app/src/main/java/com/example/property_management/ui/activities/AddPropertyActivity.java
@@ -1,8 +1,6 @@
 package com.example.property_management.ui.activities;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.Editable;
